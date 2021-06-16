e7f5937a9a8e060a0304a8780330aba43fba90f8
ressKim <sh21dlat@gmail.com>
2021-06-16 11:04:49 +0900

    load balance 적용해 보기 2

--- first-test-service/src/main/java/com/study/firsttestservice/FirstServiceController.java
+++ first-test-service/src/main/java/com/study/firsttestservice/FirstServiceController.java
@@ -1,15 +1,30 @@
 package com.study.firsttestservice;
 
+import com.netflix.discovery.converters.Auto;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/first-service/")
 @Slf4j
 public class FirstServiceController {
+
+    Environment env;
+
+    @Autowired
+    public FirstServiceController(Environment env) {
+        this.env = env;
+    }
+
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the First service.";
@@ -22,8 +37,13 @@ public class FirstServiceController {
         return "Hello message in first service";
     }
 
+    //두개 이상 실행하고 check 호출시 port 가 번갈아가며 실행되는것을 확인 해 볼 수 있다
+
     @GetMapping("/check")
-    public String check(){
-        return "Hi, there. This is a message from first service.";
+    public String check(HttpServletRequest request){
+        log.info("Server port = {}", request.getServerPort());
+        return String.format("Hi, there. This is a message from first service on port %s."
+        , env.getProperty("local.server.port"));
+
     }
 }
